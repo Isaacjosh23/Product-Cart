4773986df8175b3e9294ab3b6fe73e6713e3881b
Isaacjosh23 <isaacjoshua969@gmail.com>
2025-10-20 14:36:44 +0100

    output done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 
 const ProductContainer = () => {
   return (
-    <div className="py-8! container">
+    <div className="flex items-start py-8! container">
       <ProductDisplay />
 
       <ProductCheckout />
@@ -67,7 +67,87 @@ const AddToCartButton = () => {
 };
 
 const ProductCheckout = () => {
-  return;
+  return (
+    <div className="bg-white w-[40rem] flex flex-col gap-8 rounded-3xl py-[2rem]! px-[3rem]!">
+      <h3 className="text-3xl">Your Cart (7)</h3>
+
+      <div>
+        <div>
+          <p className="font-semibold text-[1.3rem]">Classic Tiramisu</p>
+
+          <div className="flex items-center gap-3">
+            <span className="text-[1.2rem] font-semibold text-[var(--color-red)]">
+              1x
+            </span>
+            <span className="text-[1.2rem] font-semibold text-gray-600">
+              @$5.50
+            </span>
+            <span className="text-[1.2rem] font-semibold text-gray-600">
+              $5.50
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <div>
+          <p className="font-semibold text-[1.3rem]">Classic Tiramisu</p>
+
+          <div className="flex items-center gap-3">
+            <span className="text-[1.2rem] font-semibold text-[var(--color-red)]">
+              1x
+            </span>
+            <span className="text-[1.2rem] font-semibold text-gray-600">
+              @$5.50
+            </span>
+            <span className="text-[1.2rem] font-semibold text-gray-600">
+              $5.50
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <div>
+          <p className="font-semibold text-[1.3rem]">Classic Tiramisu</p>
+
+          <div className="flex items-center gap-3">
+            <span className="text-[1.2rem] font-semibold text-[var(--color-red)]">
+              1x
+            </span>
+            <span className="text-[1.2rem] font-semibold text-gray-600">
+              @$5.50
+            </span>
+            <span className="text-[1.2rem] font-semibold text-gray-600">
+              $5.50
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <span>Order Total</span>
+
+        <p className="text-3xl font-bold">$46.50</p>
+      </div>
+
+      <div className="flex items-center justify-center gap-4 rounded-2xl bg-[var(--color-rose-50)] py-[8px]!">
+        <img src="../assets/images/icon-carbon-neutral.svg" alt="carbon icon" />
+
+        <p>
+          This is a <strong>carbon-neutral</strong> delivery
+        </p>
+      </div>
+
+      <button className="text-white cursor-pointer rounded-full font-bold text-[1.3rem] bg-[var(--color-red)] py-[8px]! px-[3rem]! hover:bg-[#8b290b] active:bg-[#8b290b] order-btn">
+        Confirm Order
+      </button>
+    </div>
+  );
 };
 
 const ProductTitle = () => {
